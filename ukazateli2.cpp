cd9863b7ae9800ca54910b68b71d8caf19f94046
Artyom <kolmazeff@yandex.ru>
2025-01-24 05:54:48 +0300

    First commmit ukazateli2

---
+++ ukazateli2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+int swap(int* a ,int* b) 
+{
+	int value1 = *a;
+	*a = *b;
+	*b = value1;
+	return 0;
+
+
+}
+
+int main()
+{
+	int a = 5, b = 8;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	swap(&a, &b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	return 0;
+}
+
+
